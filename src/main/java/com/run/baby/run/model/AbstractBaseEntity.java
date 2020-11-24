52b39e4f564bef235283ea02e804ccd667e57f1a
Natalia <tihomirova.natalia-t@yandex.ru>
2020-11-24 15:52:12 +0300

    Initial commit

---
+++ src/main/java/com/run/baby/run/model/AbstractBaseEntity.java
@@ -0,0 +1,5 @@
+package com.run.baby.run.model;
+
+public abstract class AbstractBaseEntity {
+    public abstract Integer getId();
+}
---
+++ src/main/java/com/run/baby/run/model/Client.java
@@ -0,0 +1,47 @@
+package com.run.baby.run.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Client extends AbstractBaseEntity{
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+
+    @Id
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
+
