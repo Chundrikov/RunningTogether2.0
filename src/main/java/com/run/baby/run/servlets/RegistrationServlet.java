af4a48aae76da1d64c4f4e0a49037bd5fe225256
nataliemokhnik <tihomirova.natalia-t@yandex.ru>
2021-01-03 19:56:29 +0300

    add view

---
+++ src/main/java/com/run/baby/run/servlets/RegistrationServlet.java
@@ -0,0 +1,29 @@
+package com.run.baby.run.servlets;
+
+import com.run.baby.run.model.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RegistrationServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/add.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String password = req.getParameter("pass");
+        User user = new User();
+
+        req.setAttribute("userName", name);
+        doGet(req, resp);
+    }
+}
+
---
+++ src/main/java/com/run/baby/run/web/views/search.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: chundrik
+  Date: 09.04.2020
+  Time: 00:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Найти напарника для бега</title>
+</head>
+<body>
+
+</body>
+</html>
